ce2bafb40d196fef5e64b419e6594518e5643488
Robin Berjon <robin@berjon.com>
2015-06-04 16:09:56 +0200

    outline of strategy

--- echidna-manifester.js
+++ echidna-manifester.js
@@ -1,13 +1,26 @@
 
 var Nightmare = require("nightmare")
 ,   phantomjs = require("phantomjs")
+,   u = require("url")
+,   seen = {}
 ;
-
 
 exports.run = function (path, cb) {
     var nm = new Nightmare({
         phantomPath:    pth.dirname(phantomjs.path) + "/"
     });
+    
+    nm.on("resourceRequested", function (res) {
+        var url = u.parse(res.url);
+        if (seen[url.href]) return;
+        seen[url.href] = true;
+        // XXX here process URL to filter out stuff that you don't want in the manifest
+        // e.g. ReSpec, W3C image and CSS
+    });
+    
+    // XXX
+    // how do we know that we've finished?
+    // call cb()
 };
 
 // running directly
